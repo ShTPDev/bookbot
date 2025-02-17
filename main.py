7e145e3fcb4bb1d43df4ad923f3f3fd596328ac4
Shamar Patnett <shamarp.dev@gmail.com>
2025-02-16 21:52:39 -0600

    Code update: adding charachter count funct

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 
 def word_count(file) : 
             words = file.split()
-            words = len(words)
+            return len(words)
             
 
-def charachter_count() : 
-    pass
+def charachter_count(file) : 
+    char_dic = {}
+    for char in file: 
+        lower_char = char.lower()
+        if lower_char in char_dic : 
+            char_dic[lower_char] += 1
+        else : 
+            char_dic[lower_char] = 1
+    return char_dic
+
+
+def get_book_text(path):
+    with open(path) as f:
+            return f.read()
 
 def main() :
-        with open("books/frankenstein.txt") as s:
-            file_contents = s.read()
-            word_count(file_contents)
+            bookpath = "books/frankenstein.txt"
+            text = get_book_text(bookpath)
+            count = word_count(text)
+            charachters = charachter_count(text)
+            print(charachters)
+            print(count)
         
 
         
